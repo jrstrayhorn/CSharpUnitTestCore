aef84e02cb544629356c18d98ac0f856c4af40bf
jrstrayhorn <jrstrayhorn@gmail.com>
2021-01-12 12:53:53 -0500

    first commit

---
+++ TestNinjaCore.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestNinjaCore.UnitTests
+{
+    [TestClass]
+    public class ReservationTests 
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TestNinjaCore/Fundamentals/HtmlFormatter.cs
@@ -0,0 +1,10 @@
+namespace TestNinjaCore.Fundamentals
+{
+    public class HtmlFormatter
+    {
+        public string FormatAsBold(string content)
+        {
+            return $"<strong>{content}</strong>";
+        }
+    }
+}
