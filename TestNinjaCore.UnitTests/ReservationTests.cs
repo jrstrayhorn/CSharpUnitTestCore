a1da03431a54184d83d7cd0268b80bc89d1fac09
J.R. Strayhorn <jstrayhorn@tql.com>
2021-01-15 15:14:53 -0500

    adding nUnit

--- TestNinjaCore.UnitTests/ReservationTests.cs
+++ TestNinjaCore.UnitTests/ReservationTests.cs
@@ -1,15 +1,15 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestNinjaCore.Fundamentals;
+using NUnit.Framework;
 
 namespace TestNinjaCore.UnitTests
 {
-    [TestClass]
+    [TestFixture]
     public class ReservationTests 
     {
         // usual naming convention for unit tests
         // public void MethodName_Scenario_ExpectedBehavior()
-        [TestMethod]
-        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
         {
             // Arrange - setup test
             var reservation = new Reservation();
@@ -18,7 +18,30 @@ namespace TestNinjaCore.UnitTests
             var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
 
             // Assert
+            // Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+            // Assert.That(result == true);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation() { MadeBy = user };
+            
+            var result = reservation.CanBeCancelledBy(user);
+
             Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            var reservation = new Reservation() { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
         }
     }
 }
