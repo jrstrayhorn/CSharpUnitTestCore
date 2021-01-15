be5d6323ddfa11e504618288a31470d02272eff1
J.R. Strayhorn <jstrayhorn@tql.com>
2021-01-15 15:47:24 -0500

    done - parameterized tests

--- TestNinjaCore.UnitTests/MathTests.cs
+++ TestNinjaCore.UnitTests/MathTests.cs
@@ -40,39 +40,53 @@ namespace TestNinjaCore.UnitTests
             Assert.That(result, Is.EqualTo(3));
         }
 
-        // good practice to write the method stubs w/ scenario and expected behavior
-        // that way you don't miss any scenarios/expected behavior while writing tests
+        // // good practice to write the method stubs w/ scenario and expected behavior
+        // // that way you don't miss any scenarios/expected behavior while writing tests
+        // [Test]
+        // public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        // {
+        //     var result = _math.Max(2, 1);
+
+        //     Assert.That(result, Is.EqualTo(2));
+        // }
+
+        // [Test]
+        // public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        // {
+        //     var result = _math.Max(1, 2);
+
+        //     Assert.That(result, Is.EqualTo(2));
+        // }
+
+        // // good start is stubbing out methods based on execution paths
+        // // but should also take into account the types of data that can go into
+        // // method, also the results that might come out of method
+        // // in order words black box testing
+        // // not enough to just look at execution paths
+        // // will not account for missed behavior in implementation
+        // // imagine the method is just a black box (don't just rely on written implementation)
+        // // for example, Max method take two arguments what are types of arguments
+        // // a > b, a < b, also a = b - might miss this one if only looking at implementation
+        // [Test]
+        // public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        // {
+        //     var result = _math.Max(1, 1);
+
+        //     Assert.That(result, Is.EqualTo(1));
+        // }
+
+        // can use parameterized tests
+        // can supply different arguments
+        // with this method you don't need the other Max test methods
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
         {
-            var result = _math.Max(2, 1);
+            var result = _math.Max(a, b);
 
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
-        {
-            var result = _math.Max(1, 2);
-
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        // good start is stubbing out methods based on execution paths
-        // but should also take into account the types of data that can go into
-        // method, also the results that might come out of method
-        // in order words black box testing
-        // not enough to just look at execution paths
-        // will not account for missed behavior in implementation
-        // imagine the method is just a black box (don't just rely on written implementation)
-        // for example, Max method take two arguments what are types of arguments
-        // a > b, a < b, also a = b - might miss this one if only looking at implementation
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
-        {
-            var result = _math.Max(1, 1);
-
-            Assert.That(result, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
