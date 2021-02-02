edbc1bcc865b35baf64898cd6cc2c20d4ef965c5
J.R. Strayhorn <jstrayhorn@tql.com>
2021-02-02 16:06:28 -0500

    done with DI frameworks

--- TestNinjaCore/Mocking/Program.cs
+++ TestNinjaCore/Mocking/Program.cs
@@ -6,6 +6,8 @@ namespace TestNinjaCore.Mocking
         {
             // in real world we don't manually new this up
             // we use DI framework that doesn't this for us!
+            // would need DI framework like Ninject or Autfac to get this to work
+            // via container to register dependencies
             var service = new VideoService();
             var title = service.ReadVideoTitle();
         }
--- TestNinjaCore/Mocking/VideoService.cs
+++ TestNinjaCore/Mocking/VideoService.cs
@@ -8,9 +8,13 @@ namespace TestNinjaCore.Mocking
 
         // default constructor for production
         // can use for testing
-        public VideoService(IFileReader fileReader = null)
+        // this is poor man D.I. so use a D.I. framework!!!
+        // **Ninject, StructureMap, **Autofac, Springg.Net, Unity - **recommended
+        // DI Framework sets up a container with Interface/Implementation
+        // will create object graph based on registry in container
+        public VideoService(IFileReader fileReader)
         {
-            _fileReader = fileReader ?? new FileReader();   // file reader not null use to set
+            _fileReader = fileReader;   // file reader not null use to set
         }
 
         // DI via Method Parameters, can now choose implementation to use
