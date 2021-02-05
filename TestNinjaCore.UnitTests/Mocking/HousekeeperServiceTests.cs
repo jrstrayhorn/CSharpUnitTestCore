934dbfcf21483aad77332d1c9ba6ed471bdd4b0d
J.R. Strayhorn <jstrayhorn@tql.com>
2021-02-05 17:28:40 -0500

    done - extracting helper methods

--- TestNinjaCore.UnitTests/Mocking/HousekeeperServiceTests.cs
+++ TestNinjaCore.UnitTests/Mocking/HousekeeperServiceTests.cs
@@ -10,6 +10,7 @@ namespace TestNinjaCore.UnitTests.Mocking
     [TestFixture]
     public class HousekeeperServiceTests
     {
+        private string _statementFileName;
         private HousekeeperService _service;
         private Mock<IStatementGenerator> _statementGenerator;
         private Mock<IEmailSender> _emailSender;
@@ -28,7 +29,14 @@ namespace TestNinjaCore.UnitTests.Mocking
                 _housekeeper
             }.AsQueryable());
 
+            // setup the values for happy path here
+            // if you want to test something negative change in individual test
+            _statementFileName = "filename";
             _statementGenerator = new Mock<IStatementGenerator>();
+            _statementGenerator
+                .Setup(sg => sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate))
+                .Returns(() => _statementFileName); // lazy evaluation - can change the value later, the lambda not evaluated until runtime in the test
+
             _emailSender = new Mock<IEmailSender>();
             _messageBox = new Mock<IXtraMessageBox>();
 
@@ -39,6 +47,7 @@ namespace TestNinjaCore.UnitTests.Mocking
                 _messageBox.Object);
         }
 
+        // happy path
         [Test]
         public void SendStatementEmails_WhenCalled_GenerateStatements()
         {
@@ -48,6 +57,7 @@ namespace TestNinjaCore.UnitTests.Mocking
                 sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate));
         }
 
+        // negative
         [Test]
         [TestCase(null)]
         [TestCase("")]
@@ -61,6 +71,54 @@ namespace TestNinjaCore.UnitTests.Mocking
             _statementGenerator.Verify(sg => 
                 sg.SaveStatement(_housekeeper.Oid, _housekeeper.FullName, _statementDate),
                 Times.Never);
+        }
+
+        // positive - happy path test - no need to change setup
+        [Test]
+        public void SendStatementEmails_WhenCalled_EmailTheStatement()
+        {
+            _service.SendStatementEmails(_statementDate);
+            
+            VerifyEmailSent();
+        }
+
+        
+
+        // negative - change setup that will deviate from happy path
+        // unit tests should not be long... they should be short and clean
+        // in case something breaks it doesn't take long to figure out why
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void SendStatementEmails_StatementFileNameIsNullEmptyOrWhitespace_ShouldNotEmailTheStatement(string email)
+        {
+            _statementFileName = email;
+
+            _service.SendStatementEmails(_statementDate);
+            
+            VerifyEmailNotSent();
+        }
+
+        // if this was repeated multiple times
+        // can create private helper method
+        private void VerifyEmailNotSent()
+        {
+            _emailSender.Verify(es => es.EmailFile(
+                            It.IsAny<string>(),
+                            It.IsAny<string>(),
+                            It.IsAny<string>(),
+                            It.IsAny<string>()),
+                            Times.Never);
+        }
+
+        private void VerifyEmailSent()
+        {
+            _emailSender.Verify(es => es.EmailFile(
+                            _housekeeper.Email,
+                            _housekeeper.StatementEmailBody,
+                            _statementFileName,
+                            It.IsAny<string>()));
         }
     }
 }
